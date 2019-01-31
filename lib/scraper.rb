7dc87ca76f6e2ec6416ba17e08c870cfceeccee4
Justin Davis <davisjustinw@gmail.com>
2019-01-31 06:21:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,6 +26,7 @@ class Scraper
       key = link.css('.social-icon').first['src'].slice(/.*\/(.*)-/, 1)
       key = 'blog' if key == 'rss'
       key = ":#{key}"
+      binding.pry
       hash[key] = link['href']
     end
     binding.pry
