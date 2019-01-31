70069c76f5e45640f09b866d9c20edc64dcd1c70
Justin Davis <davisjustinw@gmail.com>
2019-01-31 05:18:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,29 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    students = doc.css(".student-card")
+    students.collect do |student|
+      {
+        name: student.css('.student-name').text,
+        location: student.css('.student-location').text,
+        profile_url: student.css('a').first['href']
+      }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    social_links = doc.css('.social-icon-container a')
+    quote = doc.css('.profile-quote').text
+    bio = doc.css('.bio-block p').text
+    binding.pry
   end
 
 end
