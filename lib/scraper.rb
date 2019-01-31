524ca7176b1a9018c959a6117881e4b7fb9f8e1e
Justin Davis <davisjustinw@gmail.com>
2019-01-31 05:19:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
     social_links = doc.css('.social-icon-container a')
-    quote = doc.css('.profile-quote').text
+    profile_quote = doc.css('.profile-quote').text
     bio = doc.css('.bio-block p').text
     binding.pry
   end
